0f5f8817ed8cd54552cfed59494c59f4884c3747
pdupadhyay <pradhyumanupadhyay@gmail.com>
2021-09-25 00:02:25 +0530

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
+import { ClaimpageComponent } from './claimpage/claimpage.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginComponent } from './login/login.component';
 import { PolicypageComponent } from './policypage/policypage.component';
+import { RegisterComponent } from './register/register.component';
 import { UserpoliciesComponent } from './userpolicies/userpolicies.component';
 
 const routes: Routes = [
-  {path:"", component:UserpoliciesComponent},
-  {path:"calculate", component:CalculatorComponent},
-  {path:"buy", component:PolicypageComponent}
+  {path:"", component:HomePageComponent},
+  {path:"register", component : RegisterComponent},
+  {path:"login", component : LoginComponent},
+  {path:"estimate", component:CalculatorComponent},
+  {path:"buy", component:PolicypageComponent},
+  {path:"forgot", component:ForgotPasswordComponent},
+  {path:"renew", component:PolicypageComponent},
+  {path:"claim", component:ClaimpageComponent},
 ];
 
 @NgModule({
