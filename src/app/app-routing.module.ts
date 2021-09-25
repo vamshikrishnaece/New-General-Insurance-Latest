909065cd5350ae92966c0e5bf1d241febc8e0705
pdupadhyay <pradhyumanupadhyay@gmail.com>
2021-09-25 18:56:12 +0530

    Added more routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { ClaimpageComponent } from './claimpage/claimpage.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { FAQComponent } from './faq/faq.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginComponent } from './login/login.component';
+import { PaymentgatewayComponent } from './paymentgateway/paymentgateway.component';
 import { PolicypageComponent } from './policypage/policypage.component';
+import { PolicytypeComponent } from './policytype/policytype.component';
 import { RegisterComponent } from './register/register.component';
 import { UserpoliciesComponent } from './userpolicies/userpolicies.component';
 
@@ -16,9 +21,14 @@ const routes: Routes = [
   {path:"login", component : LoginComponent},
   {path:"estimate", component:CalculatorComponent},
   {path:"buy", component:PolicypageComponent},
-  {path:"forgot", component:ForgotPasswordComponent},
-  {path:"renew", component:PolicypageComponent},
+  {path:"login/forgot", component:ForgotPasswordComponent},
+  {path:"renew", component:PolicytypeComponent},
   {path:"claim", component:ClaimpageComponent},
+  {path:"contactus", component:ContactusComponent},
+  {path:"about", component:AboutusComponent},
+  {path:"faq", component:FAQComponent},
+  {path:"buy/type", component:PolicytypeComponent},
+  {path:"buy/type/makepayment", component:PaymentgatewayComponent}
 ];
 
 @NgModule({
